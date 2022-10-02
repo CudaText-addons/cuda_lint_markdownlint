e66544b08ac109665599996d154ed3d0486c5ed0
Alexey <support@uvviewsoft.com>
2022-10-02 11:40:21 +0300

    better find Node

--- linter.py
+++ linter.py
@@ -12,26 +12,8 @@
 """This module exports the Markdownlint plugin class."""
 
 import os
+from shutil import which
 from cuda_lint import Linter, util
-
-# https://stackoverflow.com/questions/377017/test-if-executable-exists-in-python
-def which(program):
-    import os
-    def is_exe(fpath):
-        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
-
-    fpath, fname = os.path.split(program)
-    if fpath:
-        if is_exe(program):
-            return program
-    else:
-        for path in os.environ["PATH"].split(os.pathsep):
-            exe_file = os.path.join(path, program)
-            if is_exe(exe_file):
-                return exe_file
-
-    return None
-
 
 if os.name=='nt':
     _node = 'node'
