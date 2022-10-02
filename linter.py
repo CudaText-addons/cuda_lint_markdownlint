569cf1aff03b008720e1cdf700360c1fcb12e57d
Alexey <support@uvviewsoft.com>
2022-10-02 11:22:36 +0300

    detect 'node' on unix

--- linter.py
+++ linter.py
@@ -14,7 +14,26 @@
 import os
 from cuda_lint import Linter, util
 
-_node = 'node' if os.name=='nt' else 'nodejs'
+# https://stackoverflow.com/questions/377017/test-if-executable-exists-in-python
+def which(program):
+    import os
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
+
+
+_node = 'node' if os.name=='nt' else 'node' if which('node') else 'nodejs'
 _js = os.path.join(os.path.dirname(__file__), 'node_modules', '.bin', 'markdownlint')
 
 
